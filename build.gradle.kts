f463eb2b63da85babee50821a0f660468551b33c
mingjia.ge <gemingjia0201@163.com>
2023-10-22 22:59:41 +0800

    feat:整合AOP与请求日志LogInterceptor

--- build.gradle.kts
+++ build.gradle.kts
@@ -19,6 +19,7 @@ repositories {
 }
 dependencies {
     implementation("org.springframework.boot:spring-boot-starter-web")
+    implementation("org.springframework.boot:spring-boot-starter-aop")
     implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
     implementation("org.jetbrains.kotlin:kotlin-reflect")
     // mybatis-flex kotlin扩展库
---
+++ src/main/kotlin/com/springkt/demo/aop/LogInterceptor.kt
@@ -0,0 +1,54 @@
+package com.springkt.demo.aop
+
+import org.aspectj.lang.ProceedingJoinPoint
+import org.aspectj.lang.annotation.Around
+import org.aspectj.lang.annotation.Aspect
+import org.slf4j.Logger
+import org.slf4j.LoggerFactory
+import org.springframework.stereotype.Component
+import org.springframework.util.StopWatch
+import org.springframework.web.context.request.RequestContextHolder
+import org.springframework.web.context.request.ServletRequestAttributes
+import java.util.*
+
+@Aspect
+@Component
+class LogInterceptor {
+
+    private val logger: Logger = LoggerFactory.getLogger(javaClass)
+
+    @Around("execution(* com.springkt.demo.controller.*.*(..))")
+    fun doInterceptor(point: ProceedingJoinPoint): Any? {
+        val stopWatch = StopWatch()
+        stopWatch.start()
+
+        val requestAttributes = RequestContextHolder.currentRequestAttributes() as ServletRequestAttributes
+        val httpServletRequest = requestAttributes.request
+
+        val requestId = UUID.randomUUID().toString()
+        val url = httpServletRequest.requestURI
+        val args = point.args
+        val reqParam = args?.joinToString(", ") { it.toString() } ?: "null"
+
+        logger.info(
+            "request start, id => {}, path => {}, ip => {}, params => {}",
+            requestId,
+            url,
+            httpServletRequest.remoteHost,
+            reqParam
+        )
+
+        val result: Any = try {
+            point.proceed()
+        } catch (e: Throwable) {
+            logger.error("An error occurred while processing the request: $e")
+            throw e
+        } finally {
+            stopWatch.stop()
+            val totalTimeMillis = stopWatch.totalTimeMillis
+            logger.info("request end, id => {}, cost => {}ms", requestId, totalTimeMillis)
+        }
+
+        return result
+    }
+}
